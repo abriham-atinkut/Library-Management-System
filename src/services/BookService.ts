62029201037799c64b139471a3103030e58d4871
abriham <abrihamatinkut@gmail.com>
2026-04-29 11:12:52 +0300

    feat: add BookService for creating and retrieving books
    implement BookService with add, find by id, and get all books

---
+++ src/services/BookService.ts
@@ -0,0 +1,26 @@
+import BookRepository from "../repositories/BookRepository.js";
+import type { Book } from "../models/Book.js";
+
+type CreateBookInput = Omit<Book, "id" | "isAvailable" | "createAt">;
+
+export default class BookService {
+  constructor(private bookRepo: BookRepository) {}
+  private idCounter = 3;
+
+  async add(book: CreateBookInput) {
+    return await this.bookRepo.addBook({
+      id: ++this.idCounter,
+      ...book,
+      isAvailable: true,
+      createAt: new Date(),
+    });
+  }
+
+  async getBook(id: number) {
+    return await this.bookRepo.findBookById(id);
+  }
+
+  async getAllBooks() {
+    return await this.bookRepo.loadData();
+  }
+}
