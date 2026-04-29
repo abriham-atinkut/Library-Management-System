bb2bf2c2208ade414fc93c951ee3ac7cfe44e660
abriham <abrihamatinkut@gmail.com>
2026-04-29 15:12:13 +0300

    feat: add MemberService for creating and retrieving members

---
+++ src/services/MemberService.ts
@@ -0,0 +1,26 @@
+import MemberRepository from "../repositories/MemberRepository.js";
+import type { Member } from "../models/Member.js";
+
+type CreateNewMember = Pick<Member, "name" | "email">;
+
+export default class MemberService {
+  constructor(private memberRepo: MemberRepository) {}
+  private counterId = 0;
+
+  async add(member: CreateNewMember) {
+    return await this.memberRepo.addMember({
+      id: ++this.counterId,
+      ...member,
+      joinAt: new Date(),
+      isActive: true,
+    });
+  }
+  
+  async getAllMembers() {
+    return await this.memberRepo.getMembers();
+  }
+
+  async getById(id: number) {
+    return await this.memberRepo.findMemberById(id);
+  }
+}
